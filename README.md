a3c03b44a25b8a51eead4924648b285190895cf9
Ellie Shivers <elliemshivers@gmail.com>
2022-04-28 11:13:22 -0400

    fixed vocab folder error handling

--- README.md
+++ README.md
@@ -9,11 +9,20 @@ This database does not come pre-loaded with Athena Vocabulary.
 ### Base Postgres Image
 This image is built from the base PostgreSQL image and has the same options and requirements. In practice, this typically means you must define a POSTGRES_PASSWORD. The port handling is likewise identical, using an internal port of 5432.
 
+
 ### Constraint SQL Scripts
 This project provides for a CONSTRAINTS variable which controls which scripts are ran. The default approach only runs the core OMOP 5.4 tables DDL. Setting CONSTRAINTS to "true" (case sensitive string) will load all 4 scripts. If the environment variable is not present or is set to any other value the default approach is used and constraints will not be loaded.
 
 This allows for more control over your deployment in order to speed up importing vocabulary or working in DEV environments. For example, you may wish to build the database with constraints and keys disabled, load the vocabulary, and then manually run the other SQL files afterwards to speed up the complete process.
 
+
+## Athena Vocabulary
+If you wish to preload vocabulary from Athena, you may mount a volume to the container's `/VOCAB` folder. As of this version, the approach is to this not flexible and expects all 8 CSVs provide by Athena to exist.
+
+Athena can be accessed at [https://athena.ohdsi.org/].
+
+
+## Example Deployment using Compose
 The `docker-compose.yml` provides for an example and quick deployment setting the environment variables.
 
 ```
@@ -27,4 +36,8 @@ services:
     environment:
       - POSTGRES_PASSWORD=password
       - CONSTRAINTS=true
+    volumes:
+      - ./vocab:/VOCAB
 ```
+
+For this deployment, constraint scripts will be ran and the local environment's `./vocab` folder (from the context of the build directory containing the `docker-compose.yml`) will be mapped to the `/VOCAB` folder inside of the container. If the folder contains Vocabularly CSVs from Athena, they will be loaded on database start.
--- loadDb.sh
+++ loadDb.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
-if [[ $CONSTRAINTS = "true" ]]; then
-echo "CONSTRAINTS equal true, building tables with constraints."
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE omop54;
-    \c omop54
-    \i /scripts/OMOPCDM_postgresql_5.4_ddl.sql
-    \i /scripts/OMOPCDM_postgresql_5.4_primary_keys.sql
-    \i /scripts/OMOPCDM_postgresql_5.4_indices.sql
-    \i /scripts/OMOPCDM_postgresql_5.4_constraints.sql
-EOSQL
-else
-echo "CONSTRAINTS does not equal 'true', building tables without constraints."
-echo "WARNING: If you intended to build the database with constraints, ensure your environment variable is set properly to the string 'true'."
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE omop54;
-    \c omop54
-    \i /scripts/OMOPCDM_postgresql_5.4_ddl.sql
-EOSQL
-fi
+ERROR='\033[0;31m'
+WARN='\033[1;33m'
+INFO='\033[1;32m'
+DEFAULT='\033[0m' # No Color
 
+# Step 1 - Create the initial tables at all times.
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE DATABASE omop54;
+    \c omop54
+    \i /scripts/OMOPCDM_postgresql_5.4_ddl.sql
+EOSQL
+
+
+# Step 2 - Prior to setting constraints if enabled, load vocabulary if present.
 PATH_TO_VOCAB=/VOCAB
 if [[ -d $PATH_TO_VOCAB ]]; then
-if [[ "$(ls -A $DIR)" ]]; then
+if [[ "$(ls -A $PATH_TO_VOCAB)" ]]; then
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     \c omop54
     \i /scripts/load_vocabulary.sql
 EOSQL
 else
-echo "Vocabulary Folder is Empty."
+echo -e "${ERROR}ERROR -- ${DEFAULT}Vocabulary Folder is Empty. Skipping load."
 fi
 else
-echo "Vocabulary Folder Not Found. If this is not expected, ensure that you have your local folder mounted properly and re-run the image."
+echo -e "${INFO}INFO  -- ${DEFAULT}Vocabulary Folder Not Found. If this is not expected, ensure that you have your local folder mounted properly and re-run the image."
 fi
+
+# Step 3 - If constraints are enabled, set constraints.
+if [[ $CONSTRAINTS = "true" ]]; then
+echo -e "${INFO}INFO  -- ${DEFAULT}CONSTRAINTS equal true, building tables with constraints."
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    \c omop54
+    \i /scripts/OMOPCDM_postgresql_5.4_primary_keys.sql
+    \i /scripts/OMOPCDM_postgresql_5.4_indices.sql
+    \i /scripts/OMOPCDM_postgresql_5.4_constraints.sql
+EOSQL
+else
+echo -e "${INFO}INFO  -- ${DEFAULT}CONSTRAINTS does not equal 'true', skipping setting constraints."
+fi
+
